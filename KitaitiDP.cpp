f15e940b5d636ee242af3d82f2657677f5bd9c2d
taisei <tatata0389@gmail.com>
2019-06-12 23:19:34 +0900

    期待値DP 難しかった 状態の遷移や数学的な式の変形を必要とする DPの式に自己ループが出てきたときに，式の変形をしたらよいことを学んだ 正直600点以上はありそう

--- KitaitiDP.cpp
+++ KitaitiDP.cpp
@@ -43,13 +43,53 @@ ll gcd(ll a, ll b) {
 ll lcm(ll a, ll b) {
 	return a / gcd(a, b) * b;
 }*/
+/*
+double dp[302][302][302];
+int n;
 
+double rec(int i, int j, int k) {
+	if (dp[i][j][k] >= 0) return dp[i][j][k];
+	if (i == 0 && j == 0 && k == 0) return 0.0;
+
+	double res = 0.0;
+	// 1枚のが減る
+	if (i > 0) res += rec(i - 1, j, k) * i;
+	// 2
+	if (j > 0) res += rec(i + 1, j - 1, k) * j;
+	if (k > 0) res += rec(i, j + 1, k - 1) * k;
+	res += n;
+	// 最後にまとめてかける
+	res *= 1.0 / (i + j + k);
+	return dp[i][j][k] = res;
+}
 
 int main() {
 	cin.tie(0);
 	ios::sync_with_stdio(false);
 
+	cin >> n;
+	vector<int> a(n);
+	rep(i, n) cin >> a[i];
 
+	double ans = 1.0;
+
+	// dp[i][j][k];=残りの皿の数が0枚の個数がi個，1枚がj，2枚がk個
+	// からすべての寿司がなくなるまでの操作回数の期待値
+
+	rep(i, n + 1) {
+		rep(j, n + 1) {
+			rep(k, n + 1) {
+				dp[i][j][k] = -1;
+			}
+		}
+	}
+	vector<int> num(3, 0);
+	rep(i, n) {
+		num[a[i] - 1]++;
+	}
+
+	cout << fixed << setprecision(10) << rec(num[0],num[1],num[2]) << endl;
 
 	return 0;
 }
+*/
